d591c97c42784ada43448cf0aba1f9eaee122617
Gopal Ghanghar <gopal.ghanghar@valensdatalabs.com>
2023-09-12 18:35:20 +0530

    updated date

--- dags/jaffle_shop.py
+++ dags/jaffle_shop.py
@@ -19,7 +19,7 @@ profile_config = ProfileConfig(
 
 with DAG(
         dag_id="extract_dag",
-        start_date=datetime(2023, 8, 23),
+        start_date=datetime(2023, 9, 12),
         schedule="@daily",
 ):
     e1 = EmptyOperator(task_id="pre_dbt")
